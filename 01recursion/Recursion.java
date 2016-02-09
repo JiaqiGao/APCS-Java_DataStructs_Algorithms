43a2e7dd9094e7035cb0a2502b91a85d62bef955
JiaqiGao <jiaqigao12@gmail.com>
2016-02-08 23:48:47 -0500

    Updated code to work for all test cases

--- 01recursion/Recursion.java
+++ 01recursion/Recursion.java
@@ -1,29 +1,35 @@
 public class Recursion{
     public String name(){
-        return "Gao, Jia Qi";
+        return "Gao, JiaQi";
     }
 
     public double sqrthelper(double n, double guess){
-        if (Math.pow(guess, 2)-n < 0.0000001){
+        if (Math.abs(Math.pow(guess,2)-n) < 0.00000000001){
+            // System.out.println(guess);
             return guess;
         }else{
-            return sqrthelper(n, (n/guess+guess)/2);
+            return sqrthelper(n, (n/guess+guess)/2.0);
         }
     }
         
     
     public double sqrt(double n){
+        if (n==0){
+            return 0;
+        }
         if (n<0)
             throw new IllegalArgumentException("n must be positive");
         
-        return sqrthelper(n, n/2);
+        return sqrthelper(n, 1);
     }
     /*
     public static void main(String[]arggs){
         Recursion rec = new Recursion();
         System.out.println(rec.name());
-        System.out.println(rec.sqrt(-6));
-        System.out.println(rec.sqrt(26));
+        System.out.println(rec.sqrt(7));
+        System.out.println(Math.sqrt(1.0E-7));
+        System.out.println(rec.sqrt(1.0E-7));
+        
     }
     */
     
