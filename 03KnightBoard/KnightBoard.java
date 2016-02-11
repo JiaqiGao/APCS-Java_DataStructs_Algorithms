f4b3d1ec266a87400a61b27033a5e505ecb82852
Jiaqi Gao <jiaqigao12@gmail.com>
2016-02-11 12:30:08 -0500

    Started Knight Tour problem

--- 03KnightBoard/KnightBoard.java
+++ 03KnightBoard/KnightBoard.java
@@ -1,3 +1,57 @@
 public class KnightBoard{
+    private int[][]board;
+    private int counter=0;
+
+    public KnightBoard(int size){
+	board = new int[size][size];
+    }
+ 
+    public boolean solve(){
+	return solveH(0,0);
+    }
+
+    public boolean solveH(int row, int col){
+	if(board[row][col]==1){
+	    return true; 
+	}
+	
+    }
+
+    public boolean addKnight(int row, int col){
+        for(int row=0; row<length; row++){
+            if(board[row][col] == 0){
+		counter++;
+                board[row][col] = counter;
+                if(column != length-1){
+                   
+                }
+                
+                return true;
+            }
+        }
+        return false; 
+    
+    }
+public void printSolution(){
+	/**Print the board like toString, except
+	   all negative numbers, and 0's are replaced with '_'
+	   and all 1's are replaced with 'Q'
+	 */
+        int len = board.length;
+        for(int i=0; i<len; i++){
+            for(int j=0; j<len; j++){
+                if(board[i][j]>0)
+                    System.out.print(board[i][j]);
+                if(board[i][j]<=0)
+                    System.out.print('_');
+                
+                System.out.print("       ");
+            }
+            System.out.println();
+        }
+    
+    }
+
 
 }
+
