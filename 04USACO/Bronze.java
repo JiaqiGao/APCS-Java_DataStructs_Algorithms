72c7b47359d36caac5402ab1287c59259a60a016
Jiaqi Gao <jiaqigao12@gmail.com>
2016-02-23 12:36:10 -0500

    Started problem with scanner

--- 04USACO/Bronze.java
+++ 04USACO/Bronze.java
@@ -4,12 +4,13 @@ import java.io.*;
 public class Bronze{
     
     
-    public int makelake(int R, int C, int E, int N, String map, String instructionOne, String instructionTwo){
+    public int makelake(){
 	
     }
     
     public static void main(String[]args) throws FileNotFoundException {
-	String map = new Scanner(new File(args[0])).useDelimiter("\\Z").next();
+	Scanner lines = new Scanner(makelake.in);
+	Scanner argsOne = new Scanner(lines.nextLine()); 
         
     }
 
