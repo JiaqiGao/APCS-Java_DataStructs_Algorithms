d1c8a395c5002c076303c822b0e3696b2845be97
JiaqiGao <jiaqigao12@gmail.com>
2016-02-24 01:45:24 -0500

    Working readFile

--- 04USACO/Bronze.java
+++ 04USACO/Bronze.java
@@ -20,15 +20,20 @@ public class Bronze{
             Scanner lineOne = new Scanner(lines.nextLine());
             int rows = lineOne.nextInt();
             int cols = lineOne.nextInt();
+            map = new int[rows][cols];
+            
             elevation = lineOne.nextInt();
             int numInstrucs = lineOne.nextInt();
-
+            instrucs = new int[numInstrucs][3];
+            
             for(int i=0; i<rows; i++){
                 for(int j=0; j<cols; j++){
                     map[i][j]=lines.nextInt();
+      
                 }
             }
 
+            
             for(int i=0; i<numInstrucs; i++){
                 for(int j=0; j<3; j++){
                     instrucs[i][j] = lines.nextInt();
