e388d0788c2a08c974260d8d1fed542c57321a6e
Jiaqi Gao <jiaqigao12@gmail.com>
2016-03-03 12:33:20 -0500

    Finished mergeSort but it's not void

--- 06Sorts/Sorts.java
+++ 06Sorts/Sorts.java
@@ -44,14 +44,70 @@ public class Sorts{
             System.out.println(Arrays.toString(ary));
         }
     }
-    /*
+    
+    static int[] mergeSort(int[]ary){
+	if(ary.length <= 1){
+	    return ary;
+	}
+	int[] left = new int[ary.length/2];
+	for(int i=0; i<ary.length/2; i++){
+	    left[i] = ary[i];
+	}
+	int[] right = new int [ary.length-ary.length/2];
+	int count = 0;
+	for(int i=ary.length/2; i<ary.length; i++){
+	    right[count] = ary[i];
+	    count++;
+	}
+	return merge(mergeSort(left), mergeSort(right));
+     }
+    
+    static int[] merge(int[]left, int[]right){
+	int[] acc = new int[left.length+right.length];
+	int leftcount = 0;
+	int rightcount = 0;
+	int aco = 0;
+	
+	while(leftcount < left.length && rightcount < right.length){
+  
+	    if(left[leftcount]<=right[rightcount]){
+		acc[aco] = left[leftcount];
+		leftcount++;
+	    }else{
+		acc[aco] = right[rightcount];
+		rightcount++;
+	    }
+	    aco++; 
+	    
+	}
+	if(leftcount >= left.length){
+	    for(int i=rightcount; i<right.length; i++){
+		acc[aco]=right[i];
+		aco++;
+	    }
+	}else{
+	    for(int i=leftcount; i<left.length; i++){
+		acc[aco]=left[i];
+		aco++;
+	    }
+	}
+	return acc; 
+	
+    }
+	
+	
+
+    
     public static void main(String[]args){
-	int[] tried = {2,3,4,2,1,7,5,-1,2222};
-        int[] tried2 = {2,3,4,2,1,7,5,-1,2222};
+	int[] tried = {2,7,5,-1,-2222};
+        int[] tried2 = {2,-12, 99};
+	System.out.println(Arrays.toString(mergeSort(tried)));
+	/*
         int[] tried3 = {2,3,4,2,1,7,5,-1,2222};
         selectionSort(tried);
 	bubbleSort(tried3);
         insertionSort(tried2);	
     }
     */
+    }
 }
