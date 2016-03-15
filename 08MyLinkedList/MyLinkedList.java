6b696236a3c24ae9a7b0626fa75adedb0277f701
JiaqiGao <jiaqigao12@gmail.com>
2016-03-15 12:09:08 -0400

    Update MyLinkedList.java

--- 08MyLinkedList/MyLinkedList.java
+++ 08MyLinkedList/MyLinkedList.java
@@ -1,2 +1,26 @@
-private LNode start;
-private int size;
+
+import java.util.*;
+import java.io.*;
+
+public class MyLinkedList{
+  private LNode start;
+  private int size;
+  /*
+  
+  int get(int index)- get the value of the element at the specified index (0 based)
+
+  int set(int index,int newValue) - change the value of the element at the specified index to the newValue, return the old value
+  
+  int size() - return the number of elements in the list
+  
+  int remove(int index) - remove the element at the specified index, returns the value removed
+  
+  boolean add(int index, int value) - insert a new elmeent at the specified index, 0 at the front, size() at the end.
+  
+  boolean add(int value) - adds to end
+  
+  int indexOf(int value) - returns the index of the 1st occurrence of the value in the linked list, -1 if not found.
+  
+  toString() - returns a list formatted like: [ v1, v2, v3, ... vn-1, vn ]
+*/
+}
