cbf3dd22b8df5b712677cca92db966a9075cab70
JiaqiGao <jiaqigao12@gmail.com>
2016-03-21 20:38:06 -0400

    Update MyLinkedList.java

--- 08MyLinkedList/MyLinkedList.java
+++ 08MyLinkedList/MyLinkedList.java
@@ -140,7 +140,7 @@ public class MyLinkedList<T>{
     //----------------------------------------------------
 
     public static void main(String[]args){
-	MyLinkedList<Integer> m = new MyLinkedList();
+	MyLinkedList<Integer> m = new MyLinkedList<Integer>();
 	
         m.add(5);
         m.add(11);
@@ -152,21 +152,5 @@ public class MyLinkedList<T>{
 	
     }
 
-  /*
-  
-  int get(int index)- get the value of the element at the specified index (0 based)
-
-  int set(int index,int newValue) - change the value of the element at the specified index to the newValue, return the old value
-  
-  int size() - return the number of elements in the list
-  
-  int remove(int index) - remove the element at the specified index, returns the value removed
-  
-  boolean add(int index, int value) - insert a new elmeent at the specified index, 0 at the front, size() at the end.
-  
-  
-  int indexOf(int value) - returns the index of the 1st occurrence of the value in the linked list, -1 if not found.
-
-*/
 }
 
