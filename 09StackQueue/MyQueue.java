60989dc8219b3a8afdba1e0396320793a69d7798
JiaqiGao <jiaqigao12@gmail.com>
2016-03-28 22:44:12 -0400

    Finished MyQueue

--- 09StackQueue/MyQueue.java
+++ 09StackQueue/MyQueue.java
@@ -1,29 +1,49 @@
-public class MyQueue<T> extends MyLinkedList{
-/**
+import java.util.*;
+
+public class MyQueue<T> extends MyLinkedList<T>{
+    /**
      * Adds the given item to the rear of the queue.
      */
-    void enqueue(T item);
+    public void enqueue(T item){
+        add(item);
+    }
+    
 
     /**
      * Removes the front item from the queue and returns it.
      * @exception java.util.NoSuchElementException if the queue is empty.
      */
-    T dequeue();
+    public T dequeue(){
+        if(size==0){
+            throw new NoSuchElementException();
+        }
+        T removal = remove(0);
+        return removal;
+    }
 
     /**
      * Returns the front item from the queue without popping it.
      * @exception java.util.NoSuchElementException if the queue is empty.
      */
-    T peek();
+    public T peek(){
+        if(size==0){
+            throw new NoSuchElementException();
+        }
+        return get(0);
+    }
 
     /**
      * Returns the number of items currently in the queue.
      */
-    int size();
+    public int size(){
+        return this.size();
+    }
 
     /**
      * Returns whether the queue is empty or not.
      */
-    boolean isEmpty();
-}
+    public boolean isEmpty(){
+        return size==0;
+    }
+
 }
