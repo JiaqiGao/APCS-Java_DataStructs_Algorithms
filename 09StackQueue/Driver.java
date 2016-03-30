9e67ee3e323a44b2d66e70ea59fb10b385e80151
Jiaqi Gao <jiaqigao12@gmail.com>
2016-03-30 12:30:54 -0400

    start

--- 09StackQueue/Driver.java
+++ 09StackQueue/Driver.java
@@ -3,8 +3,30 @@ import java.io.*;
 
 public class Driver{
     public static void main(String[]args){
-    
-    
+	MyQueue<String> testq = new MyQueue<String>();
+	Queue<String> origq = new LinkedList<String>();
+
+	MyStack<String> tests = new MyStack<String>();
+	Stack<String> origs = new Stack<String>();
+
+	for(int i = 0; i < 10; i++){
+            testq.enqueue(""+i);
+            origq.add(""+i);
+        }
+
+	for(int i = 0; i < 10; i++){
+            tests.push(""+i);
+            origs.add(""+i);
+        }
+	
+	System.out.println("testing MyQueue");
+	System.out.println(testq.toString(true));
+        System.out.println(origq);
+
+	System.out.println("testing MyStack");
+	System.out.println(tests.toString(true));
+        System.out.println(origs);
+
     }
     
 }
