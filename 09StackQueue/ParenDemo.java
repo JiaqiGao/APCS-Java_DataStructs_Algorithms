0e210f239825a38713307095f811c3f9b4a1950f
JiaqiGao <jiaqigao12@gmail.com>
2016-03-30 01:16:04 -0400

    Update ParenDemo.java

--- 09StackQueue/ParenDemo.java
+++ 09StackQueue/ParenDemo.java
@@ -3,21 +3,17 @@ public class ParenDemo{
     public static boolean isMatching(String s){
 	char[][] key = {{'{','}'},{'[',']'},{'(',')'},{'<','>'}};
         MyStack<Character> pancekes = new MyStack<Character>();
-        
         for(int i=0; i<s.length(); i++){   
-            
                 if(helper(s.charAt(i))==-1){
                     pancekes.push(s.charAt(i));
                 }else if(key[helper(s.charAt(i))][0] == pancekes.peek()){
                     pancekes.pop();  
                 }
-            
         }
         return pancekes.isEmpty();
     }
     
     public static int helper(char s){
-        
 	char[][] key = {{'{','}'},{'[',']'},{'(',')'},{'<','>'}};
 	for(int i=0; i<key.length; i++){
             if(key[i][0]==s){
@@ -31,19 +27,19 @@ public class ParenDemo{
     }
 
     public static void main(String[]args){
-        /*
+        
 	String input = "";
 	if(args.length>0)
 	    input = args[0];
 
 	System.out.println(isMatching(input));
        
-        */
+        /*
        
 	System.out.println(isMatching("{}{}{}"));
         System.out.println(isMatching("{}}{}"));
         System.out.println(isMatching("{}{[]}{}"));
-        
+        */
 
     }
 }
