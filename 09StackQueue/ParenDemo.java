066b8b11a1fd1833d500fb3e905504d9d14fa6fc
Jiaqi Gao <jiaqigao12@gmail.com>
2016-03-30 12:12:34 -0400

    satisfies all possibilities

--- 09StackQueue/ParenDemo.java
+++ 09StackQueue/ParenDemo.java
@@ -1,15 +1,28 @@
+import java.util.*;
+
+
 public class ParenDemo{
     
     public static boolean isMatching(String s){
 	char[][] key = {{'{','}'},{'[',']'},{'(',')'},{'<','>'}};
         MyStack<Character> pancekes = new MyStack<Character>();
         for(int i=0; i<s.length(); i++){
-	    if(helper(s.charAt(i))!= -2){
-                if(helper(s.charAt(i))==-1){
-                    pancekes.push(s.charAt(i));
-                }else if(key[helper(s.charAt(i))][0] == pancekes.peek()){
-                    pancekes.pop();  
-                }
+	    try{
+		if(helper(s.charAt(i))!= -2){
+		    if(helper(s.charAt(i))==-1){
+			pancekes.push(s.charAt(i));
+		    }else{
+			if(key[helper(s.charAt(i))][0] == pancekes.peek()){
+		   
+			pancekes.pop();  
+		    
+			}else{
+			    return false;
+			}
+		    }
+		}
+	    }catch(NoSuchElementException e){
+		return false;
 	    }
         }
         return pancekes.isEmpty();
@@ -29,19 +42,14 @@ public class ParenDemo{
     }
 
     public static void main(String[]args){
-        
-	String input = "";
-	if(args.length>0)
+	String input = "()()(([[]]))";
+	if(args.length > 0){
 	    input = args[0];
-
-	System.out.println(isMatching(input));
-       
-        /*
-       
-	System.out.println(isMatching("{}{}{}"));
-        System.out.println(isMatching("{}}{}"));
-        System.out.println(isMatching("{}{[]}{}"));
-        */
-
+	    System.out.println( isMatching(input)); 
+	}else{
+	    System.out.println("Usage:"); 
+	    System.out.println("java ParenDemo \"text\""); 
+	}
+ 
     }
 }
