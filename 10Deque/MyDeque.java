cbef78036c98f71f9a64c63efeb829708ceab0c0
Jiaqi Gao <jiaqigao12@gmail.com>
2016-04-01 12:28:15 -0400

    Started

--- 10Deque/MyDeque.java
+++ 10Deque/MyDeque.java
@@ -1,14 +1,51 @@
+import java.util.*;
+
+public class MyDeque<T>{
+    ArrayList ary = new ArrayList();
+    T start;
+    T end;
+    int size;
+
+    public MyDeque(){
+    }
+    /*
+    public String toString(){
+        return Arrays.toString(ary);
+    }
+    */
+    public void grow(){
+	ArrayList temp = new ArrayList();
+	
+    }
+    
+    public void addFirst(T value){
+	
+    }
+
+    public void addLast(T value){
+    
+    }
+
+    public T removeFirst(){
+	return start;
+    }
+
+    public T removeLast(){
+	return start;
+    }
+    
 /*For your MyDeque<T>
 0. You need a private method to grow the array and copy over the values.
 There are 6 public methods:
-1. void addFirst(T value)
-2. void addLast(T value)
+1. 
+2. 
 -When the array is full, re-size, then add. 
 -No exceptions are required since you will re-size.
-3. T removeFirst()  
-4. T removeLast()  
+3.  
+4. 
 -NoSuchElementException is thrown when there are no elements. 
 5. T getFirst()
 6. T getLast()
 -NoSuchElementException is thrown when there are no elements. 
 */
+}
