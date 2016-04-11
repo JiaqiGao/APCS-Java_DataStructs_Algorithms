efc603e6b7993bcef0e435c6d53d7fba256b7ee4
JiaqiGao <jiaqigao12@gmail.com>
2016-04-10 22:04:07 -0400

    tmrw

--- 11PathFinding/FrontierQueue.java
+++ 11PathFinding/FrontierQueue.java
@@ -1,6 +1,19 @@
 public class FrontierQueue<T> implements Frontier<T>{
+    T t;
+    
+    public void add(T element){
+    }
+    public T next(){
+        return t;
+    }
+    public boolean hasNext(){
+        return false;
+    }
     /***Make This Work This Weekend!***/
     /***You can use your classes or built in ones***/
     /***You can extend another class OR wrap around it***/
-
+    public static void main(String[] arggs){
+        System.out.println("ill do this tmrw for sure");
+    }
+    
 }
