6dfb3fc466f53b034c2e76103b86d3cfec5751b6
JiaqiGao <jiaqigao12@gmail.com>
2016-04-11 23:12:44 -0400

    Create MyQueue.java

---
+++ 11PathFinding/MyQueue.java
@@ -0,0 +1,48 @@
+import java.util.*;
+
+public class MyQueue<T> extends MyLinkedList<T>{
+    /**
+     * Adds the given item to the rear of the queue.
+     */
+    public void enqueue(T item){
+        add(item);
+    }
+    
+
+    /**
+     * Removes the front item from the queue and returns it.
+     * @exception java.util.NoSuchElementException if the queue is empty.
+     */
+    public T dequeue(){
+        if(size==0){
+            throw new NoSuchElementException();
+        }
+        return remove(0);
+    }
+
+    /**
+     * Returns the front item from the queue without popping it.
+     * @exception java.util.NoSuchElementException if the queue is empty.
+     */
+    public T peek(){
+        if(size==0){
+            throw new NoSuchElementException();
+        }
+        return get(0);
+    }
+
+    /**
+     * Returns the number of items currently in the queue.
+     */
+    public int size(){
+        return this.size();
+    }
+
+    /**
+     * Returns whether the queue is empty or not.
+     */
+    public boolean isEmpty(){
+        return size==0;
+    }
+
+}
