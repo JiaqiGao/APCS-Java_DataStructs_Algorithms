7b1e9c13d8f9940f3afa38afac16eb77e980ad3f
JiaqiGao <jiaqigao12@gmail.com>
2016-04-11 23:13:13 -0400

    Create MyStack.java

---
+++ 11PathFinding/MyStack.java
@@ -0,0 +1,47 @@
+import java.util.*;
+
+public class MyStack<T> extends MyLinkedList<T>{
+ /**
+     * Adds the given item to the top of the stack.
+     */
+    public void push(T item){
+        add(0, item);
+    }
+
+    /**
+     * Removes the top item from the stack and returns it.
+     * @exception java.util.NoSuchElementException if the queue is empty.
+     */
+    public T pop(){
+        if(size==0){
+            throw new NoSuchElementException();
+        }
+        return remove(0);
+    }
+
+    /**
+     * Returns the top item from the stack without popping it.
+     * @exception java.util.NoSuchElementException if the queue is empty.
+     */
+    public T peek(){
+        if(size==0){
+            throw new NoSuchElementException();
+        }
+        return get(0);
+    }
+
+    /**
+     * Returns the number of items currently in the stack.
+     */
+    public int size(){
+        return size;
+    }
+
+    /**
+     * Returns whether the stack is empty or not.
+     */
+    public boolean isEmpty(){
+        return size==0;
+    }
+
+}
