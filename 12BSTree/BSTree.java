16ebb2b08891d69d6f6131732cf4f5f9fe3eef75
Jiaqi Gao <jiaqigao12@gmail.com>
2016-04-20 12:28:30 -0400

    Made BSTree node

--- 12BSTree/BSTree.java
+++ 12BSTree/BSTree.java
@@ -1,1 +1,32 @@
+import java.util.*;
+import java.io.*;
 
+public class BSTree<T extends Comparable<T>>{
+    Node root;
+    
+    private class Node{
+	T data;
+	Node left, right;
+
+	public T getData(){
+	    return data;
+	}
+	public void setData(T ndat){
+	    data = ndat;
+	}	
+	public Node getLeft(){
+	    return left;
+	}
+	public void setLeft(Node ndat){
+	    left = ndat;
+	}
+	public Node getRight(){
+	    return right;
+	}
+	public void setRight(Node ndat){
+	    right = ndat;
+	}
+	
+    }
+
+}
