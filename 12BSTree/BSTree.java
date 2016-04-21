fc8ab98705ea0be8e187991b7605d57acdeb8cbd
Jiaqi Gao <jiaqigao12@gmail.com>
2016-04-21 12:31:21 -0400

    working on add

--- 12BSTree/BSTree.java
+++ 12BSTree/BSTree.java
@@ -4,19 +4,54 @@ import java.io.*;
 public class BSTree<T extends Comparable<T>>{
     Node root;
     
-    public void add(Node value){
+    /*
+    public BSTree(){
+	root = null;
+    }
+    */
+    public int getHeight(){
+	return 0;
 	
+    }
+
+    public void add(T value){
+	Node temp = new Node(value);
+	add(temp, root);
+    }
+
+    public void add(Node value, Node parent){	
+	if(parent.getLeft()==null){
+	    parent.setLeft(temp);
+	}else{
+	    if(temp.getRight()==null){
+		parent.setRight(temp);
+	    }else{
+		if(parent.getLeft().getHeight() < parent.getRight().getHeight()){
+		    add(value, parent.getLeft());
+		}
+	        add(value, parent.getRight());
+	    }
+	}
     }
     /*
     public String toString() ***
     public boolean contains(T value)
-    public int getHeight()  //this can be linear for now.
+      //this can be linear for now.
     */
-
+    
+    public T remove(T value){
+	if(!root.hasChildren()){
+	    
+	}
+    }
+    
     private class Node{
 	T data;
 	Node left, right;
-
+	
+	public Node(T value){
+	    data = value;
+	}
 	public T getData(){
 	    return data;
 	}
@@ -34,7 +69,14 @@ public class BSTree<T extends Comparable<T>>{
 	}
 	public void setRight(Node ndat){
 	    right = ndat;
-	}	
+	}
+	public boolean hasChildren(){
+	    if(left==null && right==null){
+		return false;
+	    }
+	    return true;
+	}
+	
     }
 
 }
