1707f3b1bb9fb2a07d41368a5584fc4be851eb25
JiaqiGao <jiaqigao12@gmail.com>
2016-05-04 12:20:40 -0400

    Create MyHeap.java

---
+++ 12MyHeap/MyHeap.java
@@ -0,0 +1,7 @@
+MyHeap<T extends Comparable<T>>
+//add(T)
+//T remove()
+//T peek()
+//String toString()                                                                                                                                                                                                                                 
+//MyHeap(boolean isMax) -> can be min or max
+//MyHeap() -> empty max heap
