3f9f8e0324ce6001be3321d540e17c3db18822d6
JiaqiGao <jiaqigao12@gmail.com>
2016-05-10 12:14:05 -0400

    Create RunningMedian.java

---
+++ 14RunningMedian/RunningMedian.java
@@ -0,0 +1,13 @@
+/*
+public RunningMedian()  :
+    Create an empty running median set.
+
+public double getMedian() :
+    When empty: throws new NoSuchElementException()
+    Returns the median value
+
+public void add(Integer x)
+    add to the "SmallValue" heap if  x < median, 
+    add to the "BigValue" heap otherwise. 
+    balance the two heaps so that their size differ by no more than 1. 
+*/
